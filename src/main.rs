1c61f98c4e4d9dc7504cb3d351ad27eb0357db15
Эдгар Томский <edgarkozlov3229@gmail.com>
2025-07-13 12:23:44 +0300

    Более или менее рабочая версия.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use futures_util::StreamExt;
 use itertools::Itertools;
-use reqwest::{header, Proxy};
 use reqwest::header::HeaderMap;
+use reqwest::{header, Client, Proxy};
 use scraper::Selector;
+use std::path::Path;
+use std::time::Duration;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use tokio::time::sleep;
+use url::Url;
 
-const DICT: [char; 36] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const DICT: [char; 36] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+];
 const COMB_LEN: u8 = 6;
+const DELAY: u8 = 5;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,7 +24,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut headers = HeaderMap::new();
     // headers.insert(header::HOST, "prnt.sc".parse().unwrap());
-    headers.insert(header::USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0".parse().unwrap());
+    headers.insert(
+        header::USER_AGENT,
+        "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0"
+            .parse()
+            .unwrap(),
+    );
     // headers.insert(header::ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8".parse().unwrap());
     headers.insert(header::ACCEPT_LANGUAGE, "en-US,en;q=0.5".parse().unwrap());
     // headers.insert(header::ACCEPT_ENCODING, "gzip, deflate, br, zstd".parse().unwrap());
@@ -37,58 +50,71 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .proxy(Proxy::all("socks5://127.0.0.1:2080").unwrap())
         .build()?;
 
-    let comb_iter = (0..COMB_LEN)
-        .map(|_| DICT)
-        .multi_cartesian_product();
+    let comb_iter = (0..COMB_LEN).map(|_| DICT).multi_cartesian_product();
 
+    // let mut futures = Vec::new();
     for comb_vec in comb_iter.take(100) {
+        let client = client.clone();
+        // futures.push(async move {
         let comb: String = comb_vec.into_iter().collect();
         let url = format!("https://prnt.sc/{comb}");
         println!("Getting {url}");
 
-        let text = client.get(url).send().await?.text().await?;
+        let text = client
+            .get(url)
+            .send()
+            .await?
+            // .expect("Error sending request")
+            .text()
+            .await?;
+        // .expect("Error getting text");
         // println!("{text}");
 
         let html = scraper::Html::parse_document(&text);
         let sel = Selector::parse(r#"img#screenshot-image"#).unwrap();
         if let Some(element) = html.select(&sel).next() {
             if let Some(src) = element.value().attr("src") {
-                let path = format!("img/{comb}.png");
-                println!("src: {}", src);
-
-                // let full_url = if src.starts_with("//") {
-                //     format!("https:{}", src)
-                // } else {
-                //     src.to_string()
-                // };
-                // println!("Полный URL: {}", full_url);
-
+                println!("src: {src}");
                 if src != "//st.prntscr.com/2023/07/24/0635/img/0_173a7b_211be8ff.png" {
-                    println!("{comb} found");
+                    if let Ok(url) = Url::parse(src) {
+                        let path = Path::new(url.path());
+                        let ext = path.extension().and_then(|s| s.to_str()).unwrap();
+                        let path = format!("img/{comb}.{ext}");
 
-                    let resp = client.get(src).send().await?;
-                    if !resp.status().is_success() {
-                        let error_message = format!("Ошибка: сервер ответил со статусом {}", resp.status());
-                        return Err(error_message.into());
-                    }
-
-                    let mut file = File::create(path).await?;
-                    let mut stream = resp.bytes_stream();
-
-                    while let Some(chunk_result) = stream.next().await {
-                        let chunk = chunk_result?;
-                        file.write_all(&chunk).await?;
+                        println!("{comb} found");
+                        download(client, src, &path).await?;
+                        // .expect("Download failed");
                     }
                 } else {
                     println!("{comb} not found")
                 }
-
             } else {
-                panic!("src not found")
+                println!("{comb} src not found")
             }
         } else {
-            panic!("img#screenshot-image not found.");
+            println!("img#screenshot-image not found.");
         }
+
+        sleep(Duration::from_secs(DELAY as u64)).await;
+        // });
+    }
+    // join_all(futures).await;
+
+    Ok(())
+}
+
+async fn download(client: Client, src: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let resp = client.get(src).send().await?;
+    if !resp.status().is_success() {
+        return Err(format!("Failed with status {}", resp.status()).into());
+    }
+
+    let mut file = File::create(path).await?;
+    let mut stream = resp.bytes_stream();
+
+    while let Some(chunk_result) = stream.next().await {
+        let chunk = chunk_result?;
+        file.write_all(&chunk).await?;
     }
 
     Ok(())
